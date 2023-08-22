bae0e8853cc11edc98e369cbff4f507bdcaf25e9
joseantfer <joseantfer.engineer@gmail.com>
2023-08-22 10:21:25 +0200

    Removed mesh randomization in cloth_wrap

--- app/main.cpp
+++ app/main.cpp
@@ -15,7 +15,7 @@ int main()
 	//collision_cloth_parallel_edge_test_rotation();
 	//collision_cloth_parallel_edge_test_shear();
 	//collision_cloth_parallel_edge_test_slide();
-	//cloth_wrap();
+	cloth_wrap();
 
 	//cloth_friction_slope_test();
 	//cloth_friction_corner();
@@ -36,5 +36,5 @@ int main()
 
 
 	// ------------------------- KOBUKI -------------------------
-	kobuki_test();
+	//kobuki_test();
 }
