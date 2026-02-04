297d0ef5c4f35f7061d8366b135370863ae27f93
jfernandez <jfernandez@cs.rwth-aachen.de>
2026-02-04 16:44:31 +0100

    initial state

--- stark/src/models/deformables/volume/EnergyTetStrain.cpp
+++ stark/src/models/deformables/volume/EnergyTetStrain.cpp
@@ -8,71 +8,71 @@
 stark::EnergyTetStrain::EnergyTetStrain(stark::core::Stark& stark, spPointDynamics dyn)
 	: dyn(dyn)
 {
-	stark.global_energy.add_energy("EnergyTetStrain", this->conn_complete,
-		[&](symx::Energy& energy, symx::Element& conn)
-		{
-			// Unpack connectivity
-			std::vector<symx::Index> tet = conn.slice(2, 6);
+	// stark.global_energy.add_energy("EnergyTetStrain", this->conn_complete,
+	// 	[&](symx::Energy& energy, symx::Element& conn)
+	// 	{
+	// 		// Unpack connectivity
+	// 		std::vector<symx::Index> tet = conn.slice(2, 6);
 
-			// Create symbols
-			std::vector<symx::Vector> v1 = energy.make_dof_vectors(this->dyn->dof, this->dyn->v1.data, tet);
-			std::vector<symx::Vector> x0 = energy.make_vectors(this->dyn->x0.data, tet);
-			std::vector<symx::Vector> X = energy.make_vectors(this->dyn->X.data, tet);
-			symx::Scalar scale = energy.make_scalar(this->scale, conn["group"]);
-			symx::Scalar e = energy.make_scalar(this->youngs_modulus, conn["group"]);
-			symx::Scalar nu = energy.make_scalar(this->poissons_ratio, conn["group"]);
-			symx::Scalar strain_limit = energy.make_scalar(this->strain_limit, conn["group"]);
-			symx::Scalar strain_limit_stiffness = energy.make_scalar(this->strain_limit_stiffness, conn["group"]);
-			symx::Scalar damping = energy.make_scalar(this->strain_damping, conn["group"]);
-			symx::Scalar dt = energy.make_scalar(stark.dt);
+	// 		// Create symbols
+	// 		std::vector<symx::Vector> v1 = energy.make_dof_vectors(this->dyn->dof, this->dyn->v1.data, tet);
+	// 		std::vector<symx::Vector> x0 = energy.make_vectors(this->dyn->x0.data, tet);
+	// 		std::vector<symx::Vector> X = energy.make_vectors(this->dyn->X.data, tet);
+	// 		symx::Scalar scale = energy.make_scalar(this->scale, conn["group"]);
+	// 		symx::Scalar e = energy.make_scalar(this->youngs_modulus, conn["group"]);
+	// 		symx::Scalar nu = energy.make_scalar(this->poissons_ratio, conn["group"]);
+	// 		symx::Scalar strain_limit = energy.make_scalar(this->strain_limit, conn["group"]);
+	// 		symx::Scalar strain_limit_stiffness = energy.make_scalar(this->strain_limit_stiffness, conn["group"]);
+	// 		symx::Scalar damping = energy.make_scalar(this->strain_damping, conn["group"]);
+	// 		symx::Scalar dt = energy.make_scalar(stark.dt);
 
-			// Time integration
-			std::vector<symx::Vector> x1 = time_integration(x0, v1, dt);
+	// 		// Time integration
+	// 		std::vector<symx::Vector> x1 = time_integration(x0, v1, dt);
 
-			// Scaling
-			std::vector<symx::Vector> Xs = { scale * X[0], scale * X[1], scale * X[2], scale * X[3] };
+	// 		// Scaling
+	// 		std::vector<symx::Vector> Xs = { scale * X[0], scale * X[1], scale * X[2], scale * X[3] };
 
-			// Kinematics
-			symx::Matrix DX = tet_jacobian(Xs);
-			symx::Matrix DXinv = DX.inv();
-			symx::Matrix Dx1 = tet_jacobian(x1);
-			symx::Matrix F1 = Dx1 * DXinv;
-			symx::Matrix E1 = 0.5*(F1.transpose() * F1 - energy.make_identity_matrix(3));
-			symx::Scalar tet_rest_volume = DX.det()/6.0;
+	// 		// Kinematics
+	// 		symx::Matrix DX = tet_jacobian(Xs);
+	// 		symx::Matrix DXinv = DX.inv();
+	// 		symx::Matrix Dx1 = tet_jacobian(x1);
+	// 		symx::Matrix F1 = Dx1 * DXinv;
+	// 		symx::Matrix E1 = 0.5*(F1.transpose() * F1 - energy.make_identity_matrix(3));
+	// 		symx::Scalar tet_rest_volume = DX.det()/6.0;
 
-			symx::Matrix Dx0 = symx::Matrix(symx::gather({ x0[1] - x0[0], x0[2] - x0[0], x0[3] - x0[0] }), { 3, 3 }).transpose();
-			symx::Matrix F0 = Dx0 * DXinv;
-			symx::Matrix E0 = 0.5*(F0.transpose() * F0 - energy.make_identity_matrix(3));
+	// 		symx::Matrix Dx0 = symx::Matrix(symx::gather({ x0[1] - x0[0], x0[2] - x0[0], x0[3] - x0[0] }), { 3, 3 }).transpose();
+	// 		symx::Matrix F0 = Dx0 * DXinv;
+	// 		symx::Matrix E0 = 0.5*(F0.transpose() * F0 - energy.make_identity_matrix(3));
 
-			symx::Matrix dE_dt = (E1 - E0) / dt;
+	// 		symx::Matrix dE_dt = (E1 - E0) / dt;
 
-			// [Smith et al. 2022] Stable Neo-Hookean Flesh Simulation
-			// Eq. 49 from [Smith et al. 2022]
-			symx::Scalar mu = e / (2.0 * (1.0 + nu));
-			symx::Scalar lambda = (e * nu) / ((1.0 + nu) * (1.0 - 2.0 * nu)); // 3D
-			symx::Scalar mu_ = 4.0 / 3.0 * mu;
-			symx::Scalar lambda_ = lambda + 5.0 / 6.0 * mu;
-			symx::Scalar detF = F1.det();
-			symx::Scalar Ic = F1.frobenius_norm_sq();
-			symx::Scalar alpha = 1.0 + mu_ / lambda_ - mu_ / (4.0 * lambda_);
-			symx::Scalar elastic_energy_density = 0.5 * mu_ * (Ic - 3.0) + 0.5 * lambda_ * (detF - alpha).powN(2) - 0.5 * mu_ * symx::log(Ic + 1.0);
+	// 		// [Smith et al. 2022] Stable Neo-Hookean Flesh Simulation
+	// 		// Eq. 49 from [Smith et al. 2022]
+	// 		symx::Scalar mu = e / (2.0 * (1.0 + nu));
+	// 		symx::Scalar lambda = (e * nu) / ((1.0 + nu) * (1.0 - 2.0 * nu)); // 3D
+	// 		symx::Scalar mu_ = 4.0 / 3.0 * mu;
+	// 		symx::Scalar lambda_ = lambda + 5.0 / 6.0 * mu;
+	// 		symx::Scalar detF = F1.det();
+	// 		symx::Scalar Ic = F1.frobenius_norm_sq();
+	// 		symx::Scalar alpha = 1.0 + mu_ / lambda_ - mu_ / (4.0 * lambda_);
+	// 		symx::Scalar elastic_energy_density = 0.5 * mu_ * (Ic - 3.0) + 0.5 * lambda_ * (detF - alpha).powN(2) - 0.5 * mu_ * symx::log(Ic + 1.0);
 
-			// Damping
-			symx::Scalar damping_energy_density = 0.5 * damping * dE_dt.frobenius_norm_sq();
+	// 		// Damping
+	// 		symx::Scalar damping_energy_density = 0.5 * damping * dE_dt.frobenius_norm_sq();
 
-			// Strain limiting
-			std::array<symx::Scalar, 3> s = eigenvalues_sym_3x3(E1);
-			symx::Scalar strain_limiting_energy_density = energy.make_zero();
-			for (int i = 0; i < 3; i++) {
-				symx::Scalar dl = s[i] - strain_limit;
-				strain_limiting_energy_density += symx::branch(dl > 0.0, strain_limit_stiffness*dl.powN(3)/3.0, 0.0);
-			}
+	// 		// Strain limiting
+	// 		std::array<symx::Scalar, 3> s = eigenvalues_sym_3x3(E1);
+	// 		symx::Scalar strain_limiting_energy_density = energy.make_zero();
+	// 		for (int i = 0; i < 3; i++) {
+	// 			symx::Scalar dl = s[i] - strain_limit;
+	// 			strain_limiting_energy_density += symx::branch(dl > 0.0, strain_limit_stiffness*dl.powN(3)/3.0, 0.0);
+	// 		}
 
-			// Total
-			symx::Scalar Energy = tet_rest_volume * (elastic_energy_density + damping_energy_density + strain_limiting_energy_density);
-			energy.set(Energy);
-		}
-	);
+	// 		// Total
+	// 		symx::Scalar Energy = tet_rest_volume * (elastic_energy_density + damping_energy_density + strain_limiting_energy_density);
+	// 		energy.set(Energy);
+	// 	}
+	// );
 	stark.global_energy.add_energy("EnergyTetStrain_Elasticity_Only", this->conn_elasticity_only,
 		[&](symx::Energy& energy, symx::Element& conn)
 		{
