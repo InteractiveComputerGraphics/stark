c4270c8be96bc87ee7d23e9d966556be3dad1c14
jfernandez <fernandez@cs.rwth-aachen.de>
2026-02-08 16:49:37 +0100

    wip triangle mesh minimal contact fails

--- examples/main.cpp
+++ examples/main.cpp
@@ -48,21 +48,35 @@ void hanging_cloth()
 	settings.output.codegen_directory = COMPILE_PATH;
 	settings.execution.end_simulation_time = 5.0;
 	
-	settings.simulation.init_frictional_contact = false;
+	// settings.simulation.init_frictional_contact = false;
 	settings.simulation.use_adaptive_time_step = false;
 	settings.output.fps = 120;
 	settings.simulation.max_time_step_size = 1.0/(double)settings.output.fps;
 	stark::Simulation simulation(settings);
+	const double disp = 0.1;
+
+	// Contact
+	simulation.interactions->contact->set_global_params(
+		stark::EnergyFrictionalContact::GlobalParams()
+		.set_default_contact_thickness(0.1)
+		.set_min_contact_stiffness(1e8)
+	);
 
 	// Cloth
-	const int n = 20;
+	const int n = 1;
 	const double d = 1.0;
 	const double hd = d/2.0;
 	auto [V, T, H] = simulation.presets->deformables->add_surface_grid("cloth", { d, d }, { n, n }, stark::Surface::Params::Cotton_Fabric());
+	H.point_set.add_displacement({ 0.0, 0.0, disp });
 
 	// BC
-	auto bc1 = simulation.deformables->prescribed_positions->add_inside_aabb(H.point_set, { hd, hd, 0.0 }, { 0.001, 0.001, 0.001 }, stark::EnergyPrescribedPositions::Params());
-	auto bc2 = simulation.deformables->prescribed_positions->add_inside_aabb(H.point_set, { -hd, hd, 0.0 }, { 0.001, 0.001, 0.001 }, stark::EnergyPrescribedPositions::Params());
+	auto bc1 = simulation.deformables->prescribed_positions->add_inside_aabb(H.point_set, { hd, hd, 0.0 + disp }, { 0.001, 0.001, 0.001 }, stark::EnergyPrescribedPositions::Params());
+	auto bc2 = simulation.deformables->prescribed_positions->add_inside_aabb(H.point_set, { -hd, hd, 0.0 + disp }, { 0.001, 0.001, 0.001 }, stark::EnergyPrescribedPositions::Params());
+
+	// Floor
+	auto [Vf, Tf, Hf] = simulation.presets->deformables->add_surface_grid("floor", { 10.0, 10.0 }, { 1, 1 }, stark::Surface::Params::Cotton_Fabric());
+	Hf.point_set.add_displacement({ 1.3, 2.7, 0.0 });
+	auto bc_floor = simulation.deformables->prescribed_positions->add_inside_aabb(Hf.point_set, { 0.0, 0.0, 0.0 }, { 100.0, 100.0, 100.0 }, stark::EnergyPrescribedPositions::Params());
 
 	// Run
 	simulation.run();
