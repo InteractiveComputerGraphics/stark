b263c1f033b013b7b962e56ddd492908945e8f45
jfernandez <fernandez@cs.rwth-aachen.de>
2026-05-15 08:47:32 +0200

    STARK_BUILD_EXAMPLES

---
+++ dist/build_wheel_local_linux.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Clear existing packages
+rm -f pystark/pystark/*.so
+rm -rf build dist wheelhouse *.egg-info pystark/*.egg-info
+rm -rf dist build _skbuild
+
+# Install building tools
+python -m pip install --upgrade build scikit-build-core cmake ninja
+
+# Build
+python -m build --wheel
+ls -lh dist/*.whl
